6e37e9ec9485075ec57ec5b82059cde92e6702e7
Stefano Macconi <stefano.macconi@gmail.com>
2019-09-27 16:43:08 +0200

    Waiting comp

---
+++ src/components/Waiting.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- ATTENDERE DIALOG -->
+    <wait-dialog visibile></wait-dialog>
+</template>
+
+<script>
+import WaitDialog from './WaitDialog.vue'
+
+export default {
+  components: {
+    WaitDialog
+  }
+}
+</script>
