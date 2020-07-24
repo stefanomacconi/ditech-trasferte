a1f43881a2a5a767e662e8088511ee5a9d9ef0cd
scrrnd <armando.scarpa@smeup.com>
2020-07-24 14:48:00 +0200

    gestita icona presenza allegati

--- src/components/Movimenti/Movimento.vue
+++ src/components/Movimenti/Movimento.vue
@@ -23,7 +23,7 @@
           <div>
             <v-icon v-if="movimento.conMateriale" >shopping_cart</v-icon>
             <v-icon v-if="movimento.conSpese">receipt</v-icon>
-            <!-- TODO icon allegati -->
+            <v-icon v-if="movimento.conAllegati">attach_file</v-icon>
           </div>
         </v-flex>
         <v-flex xs9 md10 :class="{'v-timeline-right-part': $vuetify.breakpoint.xs}"> <!--offset-xs1 -->
